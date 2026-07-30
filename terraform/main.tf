4943de382ddf58bd7a9088bc212e98dedeab5157
Cyberboiiii <sameer.khan.ug22@nsut.ac.in>
2026-07-30 16:30:32 +0530

    new cred.

--- terraform/main.tf
+++ terraform/main.tf
@@ -239,18 +239,22 @@ resource "aws_eks_access_policy_association" "github_actions" {
 
 
 
-resource "helm_release" "kube_prometheus_stack" {
-  name       = "kube-prometheus-stack"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack" 
-  namespace  = "monitoring"
-  
-  
-  
-  
-  set_sensitive =[ {
-    name  = "grafana.adminPassword"
-    value = var.grafana_admin_password
+resource "aws_s3_bucket" "my_bucket_microservices" {
+  bucket = "${var.project_name}-bucket"
+ force_destroy = true
+
+  tags = {
+    Name        = "${var.project_name}-bucket"
+    Environment = "Dev"
   }
-  ]
 }
+
+resource "aws_s3_bucket_public_access_block" "my_bucket_block" {
+  bucket = aws_s3_bucket.my_bucket_microservices.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
